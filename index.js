c45b8c7ec87a0746e650b54ee1cd14dadbc9b6d7
Artem Balanovsky <mr.balanovsky@gmail.com>
2022-09-01 17:15:13 +0300

    Routes added

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const homeRoutes = require('./routes/home');
+const coursesRoutes = require('./routes/courses');
+const addRoutes = require('./routes/add');
 
-const app = express(),
-    hbs = exphbs.create({
-        defaultLayout: 'main',
-        extname: 'hbs'
-    })
+const app = express();
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.render('index');
-})
-
-app.get('/about', (req, res) => {
-    res.render('about');
-})
-
-
-
+app.use('/', homeRoutes);
+app.use('/courses', coursesRoutes);
+app.use('/add', addRoutes);
 
 const PORT = process.env.PORT || 3000
 
- app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
- })
+})
