111f81813340ecc094bf044475638451532899b9
Artem Balanovsky <mr.balanovsky@gmail.com>
2022-09-07 20:40:02 +0300

    Auth session added to DB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session)
 const homeRoutes = require('./routes/home');
 const cartRoutes = require('./routes/cart');
 const coursesRoutes = require('./routes/courses');
@@ -12,9 +13,8 @@ const authRoutes = require('./routes/auth');
 const User = require('./models/user');
 const varMiddleware = require('./middleware/variables')
 
-
+const MONGODB_URI = 'mongodb+srv://Artem_Balanovskyi:QXYrAGkx2L-.5HZ@cluster.w2mzpqy.mongodb.net/shop'
 const app = express();
-
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs',
@@ -24,26 +24,33 @@ const hbs = exphbs.create({
     }
 })
 
+const store = new MongoStore({
+    collection: 'sessions',
+    uri: MONGODB_URI
+})
+
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(async (req, res, next) => {
-    try {
-        const user = await User.findById('631597b113deceb1de596e1f')
-        req.user = user
-        next()
-    } catch (e) {
-        console.log(e)
-    }
-})
+//                                                  Temporary user middleware:
+// app.use(async (req, res, next) => {
+//     try {
+//         const user = await User.findById('631597b113deceb1de596e1f')
+//         req.user = user
+//         next()
+//     } catch (e) {
+//         console.log(e)
+//     }
+// })
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
     secret: 'some secret value',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: store
 }))
 app.use(varMiddleware)
 
@@ -58,19 +65,18 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
     try {
-        const url = 'mongodb+srv://Artem_Balanovskyi:QXYrAGkx2L-.5HZ@cluster.w2mzpqy.mongodb.net/shop'
-        await mongoose.connect(url, {
+        await mongoose.connect(MONGODB_URI, {
             useNewUrlParser: true
         })
-        const candidate = await User.findOne()
-        if (!candidate) {
-            const user = new User({
-                email: 'anton@gmail.com',
-                name: 'Anton',
-                cart: { items: [] }
-            })
-            await user.save()
-        }
+        // const candidate = await User.findOne()
+        // if (!candidate) {
+        //     const user = new User({
+        //         email: 'anton@gmail.com',
+        //         name: 'Anton',
+        //         cart: { items: [] }
+        //     })
+        //     await user.save()
+        // }
 
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
