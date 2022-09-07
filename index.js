aac3a5970300baa39e7d8307d1dfe5602ed2741b
Artem Balanovsky <mr.balanovsky@gmail.com>
2022-09-07 17:01:57 +0300

    Login & Register pages added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cartRoutes = require('./routes/cart');
 const coursesRoutes = require('./routes/courses');
 const addRoutes = require('./routes/add');
 const ordersRoutes = require('./routes/orders');
+const authRoutes = require('./routes/auth');
 const User = require('./models/user');
 
 
@@ -43,6 +44,7 @@ app.use('/courses', coursesRoutes);
 app.use('/add', addRoutes);
 app.use('/cart', cartRoutes);
 app.use('/orders', ordersRoutes);
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000
 
